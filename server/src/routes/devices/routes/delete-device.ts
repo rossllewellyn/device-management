285d7b9827094176a60a47cc45efb70ae97ba1a8
Ross Llewellyn <rossllewellyn@Rosss-MacBook-Pro-4.local>
2023-05-13 14:49:54 +0100

    delete device route

---
+++ server/src/routes/devices/routes/delete-device.ts
@@ -0,0 +1,28 @@
+import { FastifyPluginAsync } from "fastify";
+import {
+  deleteDeviceBody,
+  deleteDeviceResponse,
+  DeleteDeviceRoute,
+} from "../types/delete-device-types";
+import { deviceService } from "../../../services/devices/device-service";
+
+export const deleteDeviceRoute: FastifyPluginAsync = async (app) => {
+  app.delete<DeleteDeviceRoute>(
+    "/",
+    {
+      schema: {
+        body: deleteDeviceBody,
+        response: { 200: deleteDeviceResponse },
+      },
+    },
+    async (req, res) => {
+      const response = await deleteDevice(req.body.device_id);
+
+      return res.status(200).send(response);
+    }
+  );
+};
+
+export const deleteDevice = async (deviceId: string) => {
+  return await deviceService.deleteOne(deviceId);
+};
