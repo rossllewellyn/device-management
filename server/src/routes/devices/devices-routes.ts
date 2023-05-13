67deb7705da1f5b3fbc2bd8a4bd6923b8e187a8b
Ross Llewellyn <rossllewellyn@Rosss-MacBook-Pro-4.local>
2023-05-13 12:25:04 +0100

    devices routes & types setup

---
+++ server/src/routes/devices/devices-routes.ts
@@ -0,0 +1,9 @@
+import { FastifyPluginAsync } from "fastify";
+
+export const devicesRoutes: FastifyPluginAsync = async (app) => {
+  // TODO:
+  // postDeviceRoute
+  // patchDeviceRoute
+  // postDeviceQueryRoute
+  // deleteDeviceRoute
+};
--- server/src/routes/routes-index.ts
+++ server/src/routes/routes-index.ts
@@ -3,6 +3,7 @@ import { FastifyPluginAsync } from "fastify";
 
 export const routesIndex: FastifyPluginAsync = async (app) => {
   const config = { schema: { response: { 200: T.String() } } };
+
   app.get("/health", config, async (_req, res) => {
     res.status(200).send("Looking healthy!");
   });
