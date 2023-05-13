210d7fe9651cd79e9a8545ba4102d76b3ea474c9
Ross Llewellyn <rossllewellyn@Rosss-MacBook-Pro-4.local>
2023-05-13 15:45:49 +0100

    add route

--- server/src/routes/devices/devices-routes.ts
+++ server/src/routes/devices/devices-routes.ts
@@ -2,6 +2,7 @@ import { FastifyPluginAsync } from "fastify";
 import { postDeviceRoute } from "./routes/post-device";
 import { postDeviceQueryRoute } from "./routes/post-device-query";
 import { deleteDeviceRoute } from "./routes/delete-device";
+import { patchDeviceRoute } from "./routes/patch-device";
 
 export const devicesRoutes: FastifyPluginAsync = async (app) => {
   app.register(postDeviceRoute);
@@ -10,6 +11,5 @@ export const devicesRoutes: FastifyPluginAsync = async (app) => {
 
   app.register(deleteDeviceRoute);
 
-  // TODO:
-  // patchDeviceRoute
+  app.register(patchDeviceRoute);
 };
