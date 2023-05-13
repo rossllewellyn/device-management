d5b56fedba5ce6c0f5205b49f32ef09d374c43af
Ross Llewellyn <rossllewellyn@Rosss-MacBook-Pro-4.local>
2023-05-13 14:27:50 +0100

    post device query route

---
+++ server/src/routes/devices/routes/post-device-query.ts
@@ -0,0 +1,48 @@
+import { FastifyPluginAsync } from "fastify";
+import {
+  DeviceQueryBody,
+  deviceQueryBody,
+  deviceQueryResponse,
+  PostDeviceQueryRoute,
+} from "../types/post-device-query-types";
+import { deviceService } from "../../../services/devices/device-service";
+
+export const postDeviceQueryRoute: FastifyPluginAsync = async (app) => {
+  app.post<PostDeviceQueryRoute>(
+    "/query",
+    {
+      schema: {
+        body: deviceQueryBody,
+        response: { 200: deviceQueryResponse },
+      },
+    },
+    async (req, res) => {
+      const response = await postDeviceQuery(req.body);
+
+      return res.status(200).send(response);
+    }
+  );
+};
+
+export const postDeviceQuery = async (body: DeviceQueryBody) => {
+  return deviceService.findManyFull({
+    where: {
+      OR: [
+        { device_id: { contains: body.search_text } },
+        { device_activation_code: { contains: body.search_text } },
+        { tenant_id: { contains: body.search_text } },
+        { device_make: { contains: body.search_text } },
+        { device_model: { contains: body.search_text } },
+        { device_activation_code: { contains: body.search_text } },
+        { device_os_version: { contains: body.search_text } },
+        { device_settings_json: { string_contains: body.search_text } },
+        { device_projects_json: { string_contains: body.search_text } },
+        { app_identifier: { contains: body.search_text } },
+        { app_version: { contains: body.search_text } },
+        { last_updated_ip: { contains: body.search_text } },
+        // TODO: split up above with multiple search text inputs
+        // TODO: search by created/last updated
+      ],
+    },
+  });
+};
