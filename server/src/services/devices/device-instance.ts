c38f746d5d01e6887217eaebce1f1af633ed0337
Ross Llewellyn <rossllewellyn@Rosss-MacBook-Pro-4.local>
2023-05-13 13:48:39 +0100

    device instance

---
+++ server/src/services/devices/device-instance.ts
@@ -0,0 +1,22 @@
+import { Device } from "@prisma/client";
+
+export class DeviceInstance {
+  constructor(private readonly data: Device) {}
+
+  get apiFormat() {
+    return {
+      ...this.data,
+      tenant_id: this.data.tenant_id || undefined,
+      device_make: this.data.device_make || undefined,
+      device_model: this.data.device_model || undefined,
+      device_os_version: this.data.device_os_version || undefined,
+      device_settings_json: this.data.device_settings_json || undefined,
+      device_projects_json: this.data.device_projects_json || undefined,
+      app_identifier: this.data.app_identifier || undefined,
+      app_version: this.data.app_version || undefined,
+      created_datetime: this.data.created_datetime.toISOString(),
+      last_updated_datetime: this.data.last_updated_datetime.toISOString(),
+      last_updated_ip: this.data.last_updated_ip || undefined,
+    };
+  }
+}
