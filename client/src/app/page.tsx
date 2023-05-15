611b60a3cc8370fd1b7930394a77d001e5c68c95
Ross Llewellyn <rossllewellyn@Rosss-MacBook-Pro-4.local>
2023-05-15 06:46:19 +0100

    get devices & device list state

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -4,6 +4,8 @@ import { useState } from "react";
 import styles from "./page.module.css";
 import EditDeviceForm from "./edit-device-form";
 import AddDeviceForm from "./add-device-form";
+import { postDeviceQuery } from "@/api";
+import { PostDeviceQueryResponse } from "../../../server/src/routes/devices/types/post-device-query-types";
 
 enum AppMode {
   VIEWING,
@@ -14,6 +16,13 @@ enum AppMode {
 export default function Home() {
   const [appMode, setAppMode] = useState<AppMode>(AppMode.VIEWING);
   const [searchText, setSearchText] = useState<string>("");
+  const [deviceList, setDeviceList] = useState<PostDeviceQueryResponse>([]);
+
+  const getDevices = async () => {
+    const devices = await postDeviceQuery({ search_text: searchText });
+    if (!devices) return; // TODO: add handling for this
+    setDeviceList(devices);
+  };
 
   return (
     <main className={styles.main}>
@@ -27,8 +36,12 @@ export default function Home() {
               placeholder="Search for devices ..."
               onChange={(e) => setSearchText(e.target.value)}
             />
-            <button onClick={() => {}}>Search</button>
+            <button onClick={() => getDevices()}>Search</button>
           </div>
+
+          {deviceList.map((device) => {
+            return <></>;
+          })}
         </div>
       )}
       {appMode === AppMode.EDITING && <EditDeviceForm />}
