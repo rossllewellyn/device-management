8de8f24bb2812fedbf7db223a9a7a9ea78dfa2fe
Ross Llewellyn <rossllewellyn@Rosss-MacBook-Pro-4.local>
2023-05-15 07:27:44 +0100

    add device form

--- client/src/app/add-device-form.tsx
+++ client/src/app/add-device-form.tsx
@@ -10,7 +10,7 @@ const AddDeviceForm = ({
 }) => {
   const [formData, setFormData] = useState({});
 
-  const handleFormChange = (e: ChangeEvent<HTMLInputElement>) => {
+  const handleFormChange = (e: ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
     setFormData({
       ...formData,
       [e.target.name]: e.target.value,
@@ -19,6 +19,53 @@ const AddDeviceForm = ({
 
   return (
     <form>
+      <label>
+        Device Id:
+        <input name="device_id" onChange={handleFormChange} />
+      </label>
+      <label>
+        Brand:
+        <input name="device_make" onChange={handleFormChange} />
+      </label>
+      <label>
+        Model:
+        <input name="device_model" onChange={handleFormChange} />
+      </label>
+      <label>
+        OS:
+        <input name="device_os_version" onChange={handleFormChange} />
+      </label>
+      <label>Status Code:</label>
+      <select name="device_status_code" onChange={handleFormChange}>
+        <option value="NEW">NEW</option>
+        <option value="USED">USED</option>
+        <option value="REFURBISHED">REFURBISHED</option>
+        <option value="BROKEN">BROKEN</option>
+      </select>
+      <label>
+        Tenant Id:
+        <input name="tenant_id" onChange={handleFormChange} />
+      </label>
+      <label>
+        App Identifier:
+        <input name="app_identifier" onChange={handleFormChange} />
+      </label>
+      <label>
+        App Version:
+        <input name="app_version" onChange={handleFormChange} />
+      </label>
+      <button
+        type="submit"
+        onClick={() => {
+          // these are batched together
+          setAppMode(AppMode.VIEWING);
+          addDevice({
+            ...formData,
+          });
+        }}
+      >
+        Save
+      </button>
       <button onClick={() => setAppMode(AppMode.VIEWING)}>Cancel</button>
     </form>
   );
