53038a5b441eca5f3e17ba38af3205cd4483326b
Ross Llewellyn <rossllewellyn@Rosss-MacBook-Pro-4.local>
2023-05-16 07:00:13 +0100

    server environment

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,9 +7,12 @@ const app = fastify();
 app.register(routesIndex);
 app.register(cors);
 
+const port = (process.env.SERVER_PORT as unknown as number) || 3100;
+const environment = process.env.NODE_ENV === "production" ? "0.0.0.0" : "localhost";
+
 (async () => {
   try {
-    await app.listen({ port: 3100 });
+    await app.listen({ port, host: environment });
     console.log("Server listening ...");
   } catch (error) {
     console.log(error);
